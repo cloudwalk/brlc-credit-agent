e925ff5b943578dee5e3b594c6dd4f8668fedd84
Igor Senych <igor.senych@cloudwalk.io>
2024-12-26 18:47:38 +0200

    fix: whitespaces

--- contracts/CreditAgent.sol
+++ contracts/CreditAgent.sol
@@ -77,8 +77,8 @@ contract CreditAgent is
     /// @dev The bit flags that represent the required hooks for cash-out operations.
     uint256 private constant REQUIRED_CASHIER_CASH_OUT_HOOK_FLAGS =
         (1 << uint256(ICashierHookableTypes.HookIndex.CashOutRequestBefore)) +
-            (1 << uint256(ICashierHookableTypes.HookIndex.CashOutConfirmationAfter)) +
-            (1 << uint256(ICashierHookableTypes.HookIndex.CashOutReversalAfter));
+        (1 << uint256(ICashierHookableTypes.HookIndex.CashOutConfirmationAfter)) +
+        (1 << uint256(ICashierHookableTypes.HookIndex.CashOutReversalAfter));
 
     // ------------------ Modifiers ------------------------------- //
 
